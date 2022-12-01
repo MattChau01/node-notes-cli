8b30125685dcca0d952dd198c328887fc2103161
Jeff Jin <jeffmjin@gmail.com>
2022-12-01 18:14:39 +0000

    Created a while loop.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
-const json = require('.data.json');
+const json = require('./data.json');
 fs.readFile('data.json', 'utf8', (err, data) => {
-if(err){
-  console.error(err);
-  process.exit(1);
-}
-console.log(data.notes);
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  let notes = JSON.parse(data);
+  for (let key in notes) {
+    console.log(notes[key]);
+  }
+// console.log(notes.notes);
 })
 // console.log(nodes)
+// const parse = JSON.parse('data');
+// console.log(parse);

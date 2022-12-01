96ab85f8299584cf39caa7fc978bca9acc30d70c
Randy Gerada <randygerada@gmail.com>
2022-12-01 19:02:58 +0000

    User can add a note.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const json = require('./data.json');
 
-const read = process.argv[2];
+const type = process.argv[2];
+const newNote = process.argv[3];
 
 fs.readFile('data.json', 'utf8', (err, data) => {
   if (err) {
@@ -9,21 +10,22 @@ fs.readFile('data.json', 'utf8', (err, data) => {
     process.exit(1);
   }
   let notes = JSON.parse(data);
-
   const string = JSON.stringify(notes.notes);
-  // console.log(string);
 
-  const index = Object.keys(notes.notes);
-  // console.log(index);
 
-  const value = Object.values(notes.notes);
-  // console.log(value);
-
-  for (let i = 0; i < value.length; i++) {
-    console.log(`${index[i]}: ${value[i]}`);
+  if (type === 'read') {
+    const index = Object.keys(notes.notes);
+    const value = Object.values(notes.notes);
+    for (let i = 0; i < value.length; i++) {
+      console.log(`${index[i]}: ${value[i]}`);
+    }
+  } else if (type === 'create') {
+    fs.writeFile('data.json', newNote + '\n', 'utf8', (err, data) =>{
+      if(err) {
+        console.error(err);
+        process.exit(1);
+      }
+      notes.nextId++;
+    });
   }
-
 })
-// console.log(nodes)
-// const parse = JSON.parse('data');
-// console.log(parse);

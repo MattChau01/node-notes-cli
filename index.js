71c21db0093641a9087b7f38b61dd9e4f99fc745
Matthew Chau <matthewwchau@gmail.com>
2022-12-01 18:31:43 +0000

    created variable read

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const fs = require('fs');
 const json = require('./data.json');
+
+const read = process.argv[2];
+
 fs.readFile('data.json', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   let notes = JSON.parse(data);
-  for (let key in notes) {
-    console.log(notes[key]);
+
+  const string = JSON.stringify(notes.notes);
+  // console.log(string);
+
+  const index = Object.keys(notes.notes);
+  // console.log(index);
+
+  const value = Object.values(notes.notes);
+  // console.log(value);
+
+  for (let i = 0; i < value.length; i++) {
+    console.log(`${index[i]}: ${value[i]}`);
   }
-// console.log(notes.notes);
+
 })
 // console.log(nodes)
 // const parse = JSON.parse('data');
